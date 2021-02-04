458a0328909790cfc7b94296bc9c9c45b7fd5799
jeongwon-iee <garden1117244@gmail.com>
2021-02-05 00:14:27 +0900

    [Server] feat(PostService): Add PostService

--- server/src/main/java/com/yeoun/server/module/service/PostService.java
+++ server/src/main/java/com/yeoun/server/module/service/PostService.java
@@ -1,7 +1,10 @@
 package com.yeoun.server.module.service;
 
+import com.yeoun.server.module.model.domain.Post;
 import com.yeoun.server.module.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,5 +15,12 @@ public class PostService {
 
     private final PostRepository postRepository;
 
+    public Post findOne(Long postId) {
+        return postRepository.findById(postId).orElse(null);
+    }
+
+    public Page<Post> findAll(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
 
 }
