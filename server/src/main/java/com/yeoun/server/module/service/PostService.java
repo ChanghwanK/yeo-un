94aa089585fd2fc47cc4a32a1bbcb12f4d027a45
to-0 <devchanghwan@gmail.com>
2021-02-05 02:40:07 +0900

    [Server] feat(Controller): Add PostController, PostNotFoundException

--- server/src/main/java/com/yeoun/server/module/service/PostService.java
+++ server/src/main/java/com/yeoun/server/module/service/PostService.java
@@ -1,7 +1,11 @@
 package com.yeoun.server.module.service;
 
+import com.yeoun.server.infra.exception.post.PostNotFoundException;
+import com.yeoun.server.module.dto.PostUpdateDto;
 import com.yeoun.server.module.model.domain.Post;
+import com.yeoun.server.module.model.dto.PostRequestDto;
 import com.yeoun.server.module.repository.PostRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,7 +19,7 @@ public class PostService {
 
     private final PostRepository postRepository;
 
-    public Post findOne(Long postId) {
+    public Post findById(Long postId) {
         return postRepository.findById(postId).orElse(null);
     }
 
@@ -23,4 +27,20 @@ public class PostService {
         return postRepository.findAll(pageable);
     }
 
+    public void create(PostRequestDto postRequestDto) {
+      postRepository.save(postRequestDto.toEntity(postRequestDto)) ;
+    }
+
+    public void update(PostUpdateDto updateDto, Long id) {
+      Post post = postRepository.findById(id)
+          .orElseThrow(PostNotFoundException::new);
+
+      post.update(updateDto);
+      postRepository.save(post);
+    }
+
+
+    public void delete(Long id) {
+        postRepository.deleteById(id);
+    }
 }
