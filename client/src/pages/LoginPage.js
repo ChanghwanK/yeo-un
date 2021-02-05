8f938a9b3b3bb1e2445ad43f5d42c5f30cd3ae81
yuseong01 <62369592+yuseong01@users.noreply.github.com>
2021-02-05 13:43:42 +0900

    Change LoginPage Border,Cover,input,Button Div style
    change login style

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 
 import Logo from 'components/Logo';
+import { Checkbox } from '@material-ui/core';
 
 const LoginPage = (props) => {
   const [id, setId] = useState('');
@@ -37,26 +38,24 @@ const LoginPage = (props) => {
   return (
     <Container>
       <Logo />
-      <HeaderText>Welcome :)</HeaderText>
-      <Cover>
-        <p>email</p>
-        <Input type="text" onChange={onChangeIdInput} value={id} />
-        <p>password</p>
-        <Input type="password" onChange={onChangePwInput} value={pw} />
-        <div>
-          <Button>Login</Button>
-          <Button onClick={onClickSignInButton}>Sign Up</Button>
-        </div>
-      </Cover>
+      <Border>
+        <HeaderText>Welcome to Yeoun!</HeaderText>
+        <Explain>로그인을 통해 서비스를 이용하실 수 있습니다.</Explain>
+        <Cover>
+          <p>ID</p>
+          <Input type="text" onChange={onChangeIdInput} value={id} />
+          <p>Password</p>
+          <Input type="password" onChange={onChangePwInput} value={pw} />
+
+          <div>
+            <Button>Sign in</Button>
+            <Button onClick={onClickSignInButton}>Join</Button>
+          </div>
+        </Cover>
+      </Border>
     </Container>
   );
 };
-
-const Cover = styled.div`
-  border: 5px solid black;
-  margin: 30px;
-  padding: 30px;
-`;
 
 const Container = styled.div`
   display: flex;
@@ -64,23 +63,47 @@ const Container = styled.div`
   height: 100vh;
   justify-content: center;
   align-items: center;
+`;
 
-  p {
-    margin: 10px;
-  }
+const Explain = styled.p`
+  font-size: 12px;
+`;
+
+const Border = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+  background-color: #f7f7f7;
+  border: 1px outset black;
+  border-radius: 10px;
+`;
+const Cover = styled.div`
+  margin: 30px;
+  padding: 30px;
 `;
 
 const Input = styled.input`
   border: 1px solid black;
   margin: 10px;
   padding: 10px;
+  width: 250px;
+  border-radius: 10px;
+  background-color: white;
 `;
 
 const Button = styled.button`
+  display: block;
+  width: 250px;
   border: 1px solid black;
+  border-radius: 10px;
+  background-color: #37373d;
   cursor: pointer;
   margin: 10px;
   padding: 10px;
+  text-align: center;
+  color: white;
 `;
 
 const HeaderText = styled.p`
