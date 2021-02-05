8a1e502a6a6e7b4fdbf217e7241d45df0d86cef0
jeongwon-iee <garden1117244@gmail.com>
2021-02-05 18:12:26 +0900

    [Server] fix(MemberType): Add MemberType

--- server/src/main/java/com/yeoun/server/module/model/domain/MemberType.java
+++ server/src/main/java/com/yeoun/server/module/model/domain/MemberType.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MemberType {
 
-    USER("USER", "일반 사용자"),
-    ARTIST("ARTIST", "예술가");
+    USER("ROLE_USER", "일반 사용자"),
+    ARTIST("ROLE_ARTIST", "예술가");
 
     private final String key;
     private final String title;
