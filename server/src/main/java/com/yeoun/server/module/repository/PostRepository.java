793f8f0145efcd90912759c108329133384caef3
to-0 <devchanghwan@gmail.com>
2021-02-05 14:50:32 +0900

    [Server] fix(PostServcie): 페이징 메서드 오류 수정

--- server/src/main/java/com/yeoun/server/module/repository/PostRepository.java
+++ server/src/main/java/com/yeoun/server/module/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.yeoun.server.module.repository;
 
 import com.yeoun.server.module.model.domain.Post;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
   Optional<Post> findById(Long yeounId);
-  Page<Post> findAllByCategoryId(Long categoryId);
+
+  Page<Post> findAllByCategoryId(Pageable pageable, Long categoryId);
 
 }
