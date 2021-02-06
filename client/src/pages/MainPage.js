3a93057c1f28b098a0cfae1c0d5f32d4d298439d
junghyeonsu <jung660317@naver.com>
2021-02-06 14:20:57 +0900

    [Client] Remove useEffect
    for deploy front

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import styles from 'styled-components';
-import axios from 'axios';
+// import axios from 'axios';
 
 import Header from 'components/header/Header';
 import SubHeader from 'components/sub-header/SubHeader';
@@ -11,11 +11,11 @@ import CardContext from 'contexts/CardContext';
 const MainPage = () => {
   const [state, actions] = useContext(CardContext);
 
-  useEffect(async () => {
-    await axios.get('/api/posts').then((res) => {
-      actions.setAllCard(res.data);
-    });
-  }, []);
+  // useEffect(async () => {
+  //   await axios.get('/api/posts').then((res) => {
+  //     actions.setAllCard(res.data);
+  //   });
+  // }, []);
 
   return (
     <>
