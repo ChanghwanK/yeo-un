452ef41cff11d79e51db466473f5b4fa48dbbe54
junghyeonsu <jung660317@naver.com>
2021-02-06 14:35:47 +0900

    Updates

--- server/src/main/java/com/yeoun/server/module/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.yeoun.server.module.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
